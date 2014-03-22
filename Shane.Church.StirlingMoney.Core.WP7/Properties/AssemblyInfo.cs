cd2824b60fd825911090cd95f9865d89aa6d297a
Shane Church <shane@s-church.net>
2014-03-22 16:36:42 -0600

    Updated OneDrive branding - Version 3.2 release

--- Shane.Church.StirlingMoney.Core.WP7/Properties/AssemblyInfo.cs
+++ Shane.Church.StirlingMoney.Core.WP7/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.7.*")]
-[assembly: AssemblyFileVersion("3.1.7.0")]
+[assembly: AssemblyVersion("3.2.7.*")]
+[assembly: AssemblyFileVersion("3.2.7.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
--- Shane.Church.Utility.Core/Properties/AssemblyInfo.cs
+++ Shane.Church.Utility.Core/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.7.*")]
-[assembly: AssemblyFileVersion("3.1.7.0")]
+[assembly: AssemblyVersion("3.2.7.*")]
+[assembly: AssemblyFileVersion("3.2.7.0")]
