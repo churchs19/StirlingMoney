1f3937365b202f50292632c8e656786bc144327b
Shane Church <shanec@2rm.com>
2015-10-02 16:58:33 -0500

    Prep for pushing final legacy version to Windows Store.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Shane Church")]
-[assembly: AssemblyCopyright("Copyright © Shane Church 2014")]
+[assembly: AssemblyCopyright("Copyright © Shane Church 2015")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.4.0.*")]
-[assembly: AssemblyFileVersion("3.4.0.0")]
+[assembly: AssemblyVersion("3.5.0.*")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
