6861b725bfd57d0c2bdaa82120e3b02324a36eab
Shane Church <shane@s-church.net>
2017-01-28 15:22:36 -0600

    Deleted old UWP skeleton

--- Shane.Church.StirlingMoney.UWP/LiveConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Shane.Church.StirlingMoney.UWP
-{
-	public static class LiveConfig
-	{
-		public static readonly string ClientId = "00000000400C1EB8";
-		public static readonly string[] Scopes = new string[4] { "wl.basic", "wl.offline_access", "wl.skydrive_update", "wl.emails" };
-	}
-}
